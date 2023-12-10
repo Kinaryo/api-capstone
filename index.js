e7a25c17ecd18d2a3da05eb82fa64e7faff40fc5
Kinaryo <kinaryo733huda@gmail.com>
2023-12-11 00:27:50 +0700

    husf

--- index.js
+++ index.js
@@ -76,11 +76,13 @@ app.use(session(
     }
 ))
 app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session())
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+// app.use(passport.initialize());
+// app.use(passport.session())
+// passport.use(new LocalStrategy(User.authenticate()))
+// passport.serializeUser(User.serializeUser())
+// passport.deserializeUser(User.deserializeUser())
+
+
 app.use((req,res,next)=>{
     res.locals.currentUser = req.user;
     res.locals.success_msg = req.flash('success_msg');
