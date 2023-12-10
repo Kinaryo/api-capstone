d58d55da2d4f4c5b48a0d29bba253f01285a0009
Kinaryo <kinaryo733huda@gmail.com>
2023-12-11 02:22:09 +0700

    fix

--- routes/motor.js
+++ routes/motor.js
@@ -1,41 +1,27 @@
 const express = require('express');
 const wrapAsync = require('../utils/wrapAsync');
-const constrollersMotor = require('../controllers/motor')
+const controllersMotor = require('../controllers/motor'); // Perbaikan pada penulisan 'controllersMotor'
 const ErrorHandler = require('../utils/ErrorHandler');
-const isValidObjectId = require('../middlewares/isValidObjectId')
-const isAuth = require('../middlewares/isAuth')
+const isValidObjectId = require('../middlewares/isValidObjectId');
+const isAuth = require('../middlewares/isAuth');
 const { isAuthToken } = require('../middlewares/isAuth');
-const isAuthor = require('../middlewares/isAuthor')
-const upload = require('../config/multer')
-const Motor = require('../models/motor')
-const {isAuthorMotor}  = require('../middlewares/isAuthor')
-const {validateMotor} = require('../middlewares/validator')
-// model
+const isAuthor = require('../middlewares/isAuthor');
+const upload = require('../config/multer');
+const Motor = require('../models/motor');
+const { isAuthorMotor }  = require('../middlewares/isAuthor');
+const { validateMotor } = require('../middlewares/validator');
+
 const router = express.Router();
-// schema
 
+router.get('/search', isAuthToken, wrapAsync(controllersMotor.search)); // Perbaikan pada penulisan 'controllersMotor'
+router.get('/', isAuthToken, wrapAsync(controllersMotor.index)); // Perbaikan pada penulisan 'controllersMotor'
+router.get('/detail/:id', isAuthToken, isValidObjectId('/motors'), wrapAsync(controllersMotor.detail)); // Perbaikan pada penulisan 'controllersMotor'
 
+router.get('/create/', isAuth, controllersMotor.form); // Perbaikan pada penulisan 'controllersMotor'
+router.post('/create/upload', isAuth, upload.array('image', 5), wrapAsync(controllersMotor.store)); // Perbaikan pada penulisan 'controllersMotor'
 
-router.get('/search', isAuthToken,wrapAsync(constrollersMotor.search));
-//  mendapatkan semua data motor dalam bentuk JSON
-router.get('/', isAuthToken,wrapAsync(constrollersMotor.index));
-// mendapatkan detail motor berdasarkan ID dalam bentuk JSON
-router.get('/detail/:id', isAuthToken, isValidObjectId('/motors'),wrapAsync(constrollersMotor.detail));
-// menuju halaman new data motor 
-// router.get('/create/', isAuth, constrollersMotor.form ) 
-router.get('/create/', isAuth, constrollersMotor.form ) 
-// Menambahkan data motor baru dalam bentuk JSON
-// router.post('/create/upload',isAuth, upload.single('image'),validateMotor,wrapAsync( constrollersMotor.store));
-router.post('/create/upload',isAuth, upload.array('image', 5),wrapAsync( constrollersMotor.store));
-// masuk halaman edit
-// router.get('/:id/edit',isAuth,isAuthorMotor, isValidObjectId('/motors'),wrapAsync(constrollersMotor.edit))
-router.get('/:id/edit',isAuth,isAuthorMotor, isValidObjectId('/motors'),wrapAsync(constrollersMotor.edit))
-// Mengupdate data motor berdasarkan ID dalam bentuk JSON
-// router.put('/:id/edit/update',isAuth,upload.array('image',5),isAuthorMotor,isValidObjectId('/motors'), validateMotor, wrapAsync(constrollersMotor.update));
-router.put('/:id/edit/update',isAuth,upload.array('image', 5),isValidObjectId('/motors'), validateMotor, wrapAsync(constrollersMotor.update));
-// menghapus data motor berdasarkan ID dalam bentuk JSON
-// router.delete('/:id',isAuth,isAuthorMotor,isValidObjectId('/motors'), wrapAsync(constrollersMotor.destroy));
-// menghapus data motor berdasarkan ID dalam bentuk JSON
-router.delete('/:id/deleted',isAuth,isAuthorMotor,isValidObjectId('/motors'), wrapAsync(constrollersMotor.destroy));
+router.get('/:id/edit', isAuth, isAuthorMotor, isValidObjectId('/motors'), wrapAsync(controllersMotor.edit)); // Perbaikan pada penulisan 'controllersMotor'
+router.put('/:id/edit/update', isAuth, upload.array('image', 5), isValidObjectId('/motors'), validateMotor, wrapAsync(controllersMotor.update)); // Perbaikan pada penulisan 'controllersMotor'
+router.delete('/:id/deleted', isAuth, isAuthorMotor, isValidObjectId('/motors'), wrapAsync(controllersMotor.destroy)); // Perbaikan pada penulisan 'controllersMotor'
 
 module.exports = router;
