d345e11cd70a6bb71ab01e890bddb0820c7cc2bf
Kinaryo <kinaryo733huda@gmail.com>
2023-12-11 02:42:13 +0700

    hufs

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,19 @@
 const express = require('express');
-const router = express();
+const router = express.Router();  // Use express.Router() to create a router
 const User = require('../models/user');
 const wrapAsync = require('../utils/wrapAsync');
-const { route } = require('./motor');
 const passport = require('passport');
-const controllersAuth = require('../controllers/auth')
+const controllersAuth = require('../controllers/auth');
 const bcrypt = require('bcrypt');
-
 
 // Register
 // router.get('/register', controllersAuth.registerForm);
-
 router.post('/register', wrapAsync(controllersAuth.register));
 
 // Login
 // router.get('/login', controllersAuth.loginForm);
+router.post('/login', controllersAuth.login);
 
-router.post('/login', controllersAuth.login )
 // Logout
 router.post('/logout', controllersAuth.logout);
 
