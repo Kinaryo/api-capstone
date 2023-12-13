64cf71d6b187b8c268e758e6639ee0f7f1430de2
Kinaryo <kinaryo733huda@gmail.com>
2023-12-13 20:23:27 +0700

    pindah ke server dari lokal

--- index.js
+++ index.js
@@ -19,26 +19,26 @@ app.use(express.json());
 
 
 // Connect to MongoDB
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         });
-//         console.log(`MongoDB Connected`);
-//     } catch (error) {
-//         console.error('Error connecting to MongoDB:', error.message);
-//         process.exit(1);
-//     }
-// };
-// connectDB();
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log(`MongoDB Connected`);
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1);
+    }
+};
+connectDB();
 
-mongoose.connect('mongodb://127.0.0.1/motositefinder')
-.then((result)=>{
-    console.log('connected to mongodb')
-}).catch((err)=>{
-    console.log(err)
-})
+// mongoose.connect('mongodb://127.0.0.1/motositefinder')
+// .then((result)=>{
+//     console.log('connected to mongodb')
+// }).catch((err)=>{
+//     console.log(err)
+// })
 // Other imports...
 
 // Middleware setup...
@@ -87,11 +87,11 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server...
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Listening On Port ${PORT}`);
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening On Port ${PORT}`);
+});
 
-app.listen(5000,()=>{
-    console.log(`server is running on http://127.0.0.1:5000`)
-})
+// app.listen(5000,()=>{
+//     console.log(`server is running on http://127.0.0.1:5000`)
+// })
